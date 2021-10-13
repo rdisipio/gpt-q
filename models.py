9b2520a2219574e1d49ae770fd13bc2e95d3d9f5
Riccardo Di Sipio <disipio@Riccardos-MacBook-Pro-2.local>
2021-10-13 09:35:04 -0400

    adding feedforward

--- models.py
+++ models.py
@@ -44,8 +44,7 @@ class QConv1d(pl.LightningModule):
         batch_size, seq_len, embed_dim = x.shape
         x = F.pad(x, (self.padding, self.padding), "constant", 0)
         out_dim = int((embed_dim + 2 * self.padding - self.kernel_size) / self.stride) + 1
-        output = [torch.zeros((batch_size, seq_len, out_dim)) for _ in range(self.out_channels)]
-        
+        output = torch.zeros((batch_size, seq_len, out_dim, self.out_channels))
         for i in range(batch_size):
             for j in range(seq_len):
                 for k in range(0, out_dim, self.stride):
@@ -53,6 +52,43 @@ class QConv1d(pl.LightningModule):
                     x_slice  = x[i, j, k:k_end]
                     q_result = self.qconv(x_slice)
                     for c in range(self.out_channels):
-                        output[c][i, j, k] = q_result[c]
+                        output[i, j, k, c] = q_result[c]
+                    #    output[c][i, j, k] = q_result[c]
         return output
 
+
+class FeedForward(pl.LightningModule):
+    '''
+    Used to create contextualized embeddings from the attention heads
+    '''
+    def __init__(self,
+                 embed_dim,
+                 boom_factor=4,
+                 dropout=0.1,
+                 kernel_size=5):
+        super(FeedForward, self).__init__()
+        assert kernel_size % 2 == 1, "Kernel size must be odd to conserve embedding dimension"
+        padding = (kernel_size - 1) // 2
+
+        self.c_fc = QConv1d(kernel_size, out_channels=boom_factor, padding=padding)
+
+        s_inv = (boom_factor * embed_dim - boom_factor) // (embed_dim - 1)
+        self.c_proj = QConv1d(kernel_size=boom_factor, out_channels=1, stride=s_inv)
+        self.activation = F.gelu
+        self.dropout = nn.Dropout(dropout)
+ 
+    def _scatter_and_merge(self, x):
+        batch_sz, seq_len, embed_dim, n_ch = x.shape
+        x = x.transpose(-1,-2)
+        x = x.view(batch_sz, seq_len, n_ch, -1)
+        x = x.reshape((batch_sz, seq_len, n_ch * embed_dim))
+        return x
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self._scatter_and_merge(x)
+        x = self.activation(x)
+        x = self.c_proj(x)
+        x = self.dropout(x)
+        return x
+        #return self.dropout(self.c_proj(self.act(self.c_fc(x))))
